dad395afb32f006b4ed35ebcc1aaa0baf142d73c
juanfisicobr <128095379+juanfisicobr@users.noreply.github.com>
2023-04-06 16:49:39 -0300

    updd

--- client/index.js
+++ client/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
+
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+  <ApolloProvider client={client}>
+    <SongList/>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(
